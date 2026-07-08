f81ea6b472431d64bac2671d3ba7f4e61e6df28d
Diego Alvarez <diegoalvarezc95@gmail.com>
2026-07-07 20:42:07 -0500

    changed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://diego-portfolio-ruby.vercel.app',
-  outDir: '/tmp/dist',
   cacheDir: '/tmp/vite-cache',
   vite: { cacheDir: '/tmp/vite-cache' },
 });
